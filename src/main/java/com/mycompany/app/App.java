a77d9c482a2844d1e2296d11cef9eb5e802a148f
ilkerpolat6 <ilkerpolat3@gmail.com>
2018-10-10 01:53:37 +0300

    bitti galiba

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,29 +12,25 @@ import spark.template.mustache.MustacheTemplateEngine;
 public class App {
     public static String search(ArrayList<Integer> array, int x, int y, int z) {
         System.out.println("\n*\ninside search");
-        String res="densities:";
-        for(int i:array){
-            int a=i/(x*y*z);
-            res=res+" "+a;
+        String res = "densities:";
+        for (int i : array) {
+            int a = i / (x * y * z);
+            res = res + " " + a;
         }
         System.out.println(res);
         return res;
     }
 
     public static void main(String[] args) {
-        System.out.println("main e girdik");
         port(getHerokuAssignedPort());
         get("/", (req, res) -> "ilker polat");
         post("/compute", (req, res) -> {
             // System.out.println(req.queryParams("input1"));
             // System.out.println(req.queryParams("input2"));
-            System.out.println("besmele");
             String input1 = req.queryParams("input1");
             java.util.Scanner sc1 = new java.util.Scanner(input1);
-            System.err.println("ıııııı");
             sc1.useDelimiter("[;\r\n]+");
             java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
-            System.out.println("1 scanlendin");
             while (sc1.hasNext()) {
                 int value = Integer.parseInt(sc1.next().replaceAll("\\s", ""));
                 inputList.add(value);
@@ -42,13 +38,10 @@ public class App {
             System.out.println(inputList);
             String input2 = req.queryParams("input2").replaceAll("\\s", "");
             int input2AsInt = Integer.parseInt(input2);
-            System.out.println("2demek almışız");
             String input3 = req.queryParams("input3").replaceAll("\\s", "");
             int input3AsInt = Integer.parseInt(input3);
-            System.out.println("3demek almışız");
             String input4 = req.queryParams("input4").replaceAll("\\s", "");
             int input4AsInt = Integer.parseInt(input4);
-            System.out.println("4demek almışız");
             String result = App.search(inputList, input2AsInt, input3AsInt, input4AsInt);
             Map map = new HashMap();
             map.put("result", result);
@@ -56,7 +49,7 @@ public class App {
         }, new MustacheTemplateEngine());
         get("/compute", (rq, rs) -> {
             Map map = new HashMap();
-            map.put("result", "nnot computed yet!");
+            map.put("result", "not computed yet!");
             return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
     }
@@ -68,4 +61,5 @@ public class App {
         }
         return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
     }
+
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -3,7 +3,8 @@ package com.mycompany.app;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 /**
  * Unit test for simple App.
  */
@@ -35,4 +36,42 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+
+    public void testIntDiv() {
+        App rlc= new App();
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        String res=rlc.search(array, 99,99,99);
+        boolean boo=res.equals("densities: 0 0 0 0");
+        assertTrue(boo);
+    }
+    public void test1Div() {
+        App rlc= new App();
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        String res=rlc.search(array, 1,1,1);
+        boolean boo=res.equals("densities: 1 2 3 4");
+        assertTrue(boo);
+    }
+    public void test0Div() {
+        App rlc= new App();
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(0,0,0,0));
+        String res=rlc.search(array,1,100,500);
+        boolean boo=res.equals("densities: 0 0 0 0");
+        assertTrue(boo);
+    }
+    public void testSingleMass() {
+        App rlc= new App();
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(128));
+        String res=rlc.search(array,2,2,2);
+        boolean boo=res.equals("densities: 16");
+        assertTrue(boo);
+    }
+    public void testnoneMass() {
+        App rlc= new App();
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList());
+        String res=rlc.search(array,2,2,2);
+        boolean boo=res.equals("densities:");
+        assertTrue(boo);
+    }
+
 }
